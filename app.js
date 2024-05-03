18c80cc0a6b93d9938fcac4b4b67b87bdc0535e4
Miroslav Tsarev <mtzarev@gmail.com>
2024-05-03 23:45:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function attachGradientEvents() {
     function gradientMove(event) {
     let power = event.offsetX / (event.target.clientWidth - 1);
     power = Math.trunc(power * 100);
-      document.getElementById('result').textContent = power + "%";
+    document.getElementById('result').textContent = power + "%";
     }
     function gradientOut(event) {
       document.getElementById('result').textContent = "";
